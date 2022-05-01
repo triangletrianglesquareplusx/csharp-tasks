0e650169af8c59675c664e70f27a27a06588bb01
triangletrianglesquareplusx <preslavgetov@gmail.com>
2022-05-01 15:17:47 +0300

    uploading solutions

---
+++ task1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //read the JSON 
+            string fileToDesirialize = @"C:\Users\Preslav\Downloads\task1\task1\task1\sample.txt";
+            
+            string jsonText = File.ReadAllText(fileToDesirialize);
+            var data = JsonConvert.DeserializeObject<MovieStar[]>(jsonText);
+            // Create a pattern for nums  
+            string pattern = @"\b\d+\b";
+            // Create a Regex  
+            Regex rg = new Regex(pattern);
+            //create regex
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.Name} {item.Surname}");
+                Console.WriteLine($"{item.Sex}");
+                Console.WriteLine($"{item.Nationality}");
+                
+                string result = rg.Matches(item.dateOfBirth)[1].Value;
+                Console.WriteLine($"{DateTime.Now.Year - int.Parse(result)} years old");
+                Console.WriteLine();
+            }
+            
+        }
+    }
+    //I am aware that classes usally stay within their own files. For bravity sake I have decided to include the class within the same file. 
+    public class MovieStar
+    {
+        public string dateOfBirth { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Sex { get; set; }
+        public string Nationality { get; set; }
+    }
+}
---
+++ task2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputGrossSalary = int.Parse(Console.ReadLine());
+            double netSalary = 0;
+            if (inputGrossSalary <= 1000)
+            {
+                netSalary = inputGrossSalary;
+            }
+            else
+            {
+                double taxableAmount = inputGrossSalary - 1000;
+                double incomeTax = taxableAmount * 0.1;
+                double socialContribution = 0;
+              
+                if (isValueAboveTreeK(inputGrossSalary))
+                {
+                    //here the user should contribute social tax - how much??
+                    socialContribution = (taxableAmount - (inputGrossSalary - 3000)) * 0.15;
+                    netSalary = inputGrossSalary - (incomeTax + socialContribution);
+                    
+                }
+                else
+                {
+                    socialContribution = taxableAmount * 0.15;
+                    netSalary = inputGrossSalary - (incomeTax + socialContribution);
+                }
+                
+            }
+            Console.WriteLine(netSalary);
+            //this is where I want to mention that I have no idea what you mean by "how new taxes can be implemented". Sorry about that. 
+        }
+        
+        static bool isValueAboveTreeK(double value)
+        {
+            return value > 3000;
+        }
+       
+
+    }
+}
